2298e1bde41cb4c96ee3318be6fe0cdd09b0a361
sakozo <youlianggit@gmail.com>
2020-02-20 00:50:10 +0900

    デプロイエラーの原因と思われる記述をコメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ApplicationRecord
-  has_many :item_images
+  #has_many :item_images
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,4 +1,4 @@
 class ItemImage < ApplicationRecord
-  mount_uploader :src,image, ImageUploader
+  #mount_uploader :src,image, ImageUploader
   belongs_to :item
 end
