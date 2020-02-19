2a609cb2163c9e655f3344107b299922a5ab0e15
yasutaka <siesta27@gmail.com>
2020-02-20 01:17:30 +0900

    プライベートメソッドの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,4 +11,12 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def create
+    Item.create(item_params)
+  end
+  private
+  def item_params
+    params.require(:item).permit(:name,:description,:status,:is_bear_shipping_cost,:region,:period,:price,:selling_status,:category_id,:brand_id,item_images_attributes: [:image]).marge(user_id:current_user.id)
+  end
+
 end
