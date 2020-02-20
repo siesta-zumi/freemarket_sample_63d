a3c3f177826b70558e0c826feb7bbfd9bbce0b75
yasutaka <siesta27@gmail.com>
2020-02-20 19:37:12 +0900

    mount_uploaderの修正

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,4 +1,10 @@
 class ItemImage < ApplicationRecord
+<<<<<<< HEAD
+  mount_uploader :image, ImageUploader
+=======
+
   mount_uploader :src,image, ImageUploader
+>>>>>>> 9f7a493273cfcb7d34c534092b09ce5eeb42c7ba
   belongs_to :item, optional: true
+
 end
