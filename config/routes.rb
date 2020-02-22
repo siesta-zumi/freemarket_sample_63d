32cce58d0af3ba6142ccce06832c8748da0ee6f9
yasutaka <siesta27@gmail.com>
2020-02-22 14:55:11 +0900

    ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root "items#index"
-  resources :items, only: [:index, :new,:show]
+  resources :items, only: [:index, :new,:show,:create]
 
 
 end
