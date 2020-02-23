2514072aeb859d12d9230cec925d4aa82f85d072
sakozo <youlianggit@gmail.com>
2020-02-23 15:08:29 +0900

    レビュー指摘事項修正(2)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,25 +1,24 @@
 class CardsController < ApplicationController
   before_action :set_card, only: [:new, :delete, :show]
-  require "payjp"
 
   def new
-    redirect_to action: "show" if !@card.blank? #変数@cardに情報が入っていた場合、確認画面用のshowアクションにリダイレクト
+    redirect_to card_path(current_user.card.id) if !@card.blank? #変数@cardに情報が入っていた場合、確認画面用のshowアクションにリダイレクト
   end
 
   def pay #payjpとCardsテーブルに登録を行うアクション
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     if params['payjp-token'].blank?
-      redirect_to action: "new"
+      redirect_to new_card_path
     else
       customer = Payjp::Customer.create( #顧客idの作成を行う
-      card: params['payjp-token'],
-      metadata: {user_id: current_user.id}
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
       )
       card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if card.save               #Cardsテーブルに登録
-        redirect_to action: "show"
+        redirect_to card_path(current_user.card.id)
       else
-        redirect_to action: "pay"
+        redirect_to pay_cards_path
       end
     end
   end
@@ -31,12 +30,12 @@ class CardsController < ApplicationController
       customer.delete
       @card.delete
     end
-      redirect_to action: "new"
+      redirect_to new_card_path
   end
 
   def show #カードのデータpayjpに送り情報を取り出します
     if @card.blank?
-      redirect_to action: "new" 
+      redirect_to new_card_path 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
