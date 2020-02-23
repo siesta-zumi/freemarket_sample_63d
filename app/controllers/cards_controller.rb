a4ff799ba097761bc97405a588e6245eafc469a8
sakozo <youlianggit@gmail.com>
2020-02-23 14:25:09 +0900

    レビュー指摘事項修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,9 @@
 class CardsController < ApplicationController
-
+  before_action :set_card, only: [:new, :delete, :show]
   require "payjp"
 
   def new
-    card = Card.where(user_id: current_user.id) #Cardsテーブルからユーザーidに紐づいたカードが登録されていた場合取得
-    redirect_to action: "show" if card.exists? #変数cardに情報が入っていた場合、確認画面用のshowアクションにリダイレクト
+    redirect_to action: "show" if !@card.blank? #変数@cardに情報が入っていた場合、確認画面用のshowアクションにリダイレクト
   end
 
   def pay #payjpとCardsテーブルに登録を行うアクション
@@ -16,8 +15,8 @@ class CardsController < ApplicationController
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       )
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save               #Cardsテーブルに登録
+      card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if card.save               #Cardsテーブルに登録
         redirect_to action: "show"
       else
         redirect_to action: "pay"
@@ -26,26 +25,29 @@ class CardsController < ApplicationController
   end
 
   def delete #payjpとCardsテーブルから削除を行うメソッド
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-    else
+    if !@card.blank?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
-      card.delete
+      @card.delete
     end
       redirect_to action: "new"
   end
 
   def show #カードのデータpayjpに送り情報を取り出します
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
+    if @card.blank?
       redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
+  end
+
+  private
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first #Cardsテーブルからユーザーidに紐づいたカード情報を取得
   end
 
 end
