a4ed7a38c7de8fd0bb63acbd91c7fde2d55360e8
yasutaka <siesta27@gmail.com>
2020-02-27 20:37:18 +0900

    seedデータの入力

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#親要素
+lady = Category.create(:name=>"レディース")
+
+#子要素
+lady_tops = lady.children.create(:name=>"トップス")
+lady_jacket = lady.children.create(:name=>"ジャケット/アウター")
+
+#孫要素（childrenと記述することで直前の変数の子要素として扱うことができる）
+lady_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"その他"}])
+lady_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"その他"}])
