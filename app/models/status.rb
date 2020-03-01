85422a95360b66171eb881a68d902dd17af3ab84
sakozo <youlianggit@gmail.com>
2020-03-01 15:36:11 +0900

    インデント修正

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ActiveHash::Base
   self.data = [
-      {id: 1, name: '新品'}, {id: 2, name: '中古'}, {id: 3, name: '傷あり'}
+    {id: 1, name: '新品'}, {id: 2, name: '中古'}, {id: 3, name: '傷あり'}
   ]
 end
