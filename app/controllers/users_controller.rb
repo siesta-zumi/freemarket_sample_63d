90d779c30ebfd771ca55e806e790412ecf5a53a8
chiwawa313 <no_pain.no_gain.0313@docomo.ne.jp>
2020-03-04 21:26:00 +0900

    set_userメゾット追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show] 
   before_action :set_parents, only: [:index,:new,:show] #applicationControllerを継承しているのでこのコントローラで定義しなくてもset_paramsメソッドは使えます。
 
   def show
-    @user = User.find(params[:id])
     @items = @user.items
   end
+
+  private 
   
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
