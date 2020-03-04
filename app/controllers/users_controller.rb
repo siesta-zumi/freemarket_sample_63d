5ecc1373d323c2cac69f084ca339e53d88aa8017
chiwawa313 <no_pain.no_gain.0313@docomo.ne.jp>
2020-03-04 20:24:06 +0900

    user_controller修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 before_action :set_parents, only: [:index,:show]
   def show
     @user = User.find(params[:id])
-    @items =Item.where(user: current_user.id)
+    @items = @user.items
   end
+  
 end
