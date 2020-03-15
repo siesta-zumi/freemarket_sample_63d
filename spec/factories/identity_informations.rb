60cd76b296cb2c52479910c276d9c05227b80141
sakozo <youlianggit@gmail.com>
2020-03-15 09:13:51 +0900

    テストコード作成

--- spec/factories/identity_informations.rb
+++ spec/factories/identity_informations.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
 
   factory :identity_information do
     user_id               {""}
-    first_name            {"橋ノ本"}
-    last_name             {"環奈子"}
-    first_name_furigana   {"はしのもと"}
-    last_name_furigana    {"かんなこ"}
+    first_name            {"環奈子"}
+    last_name             {"橋ノ本"}
+    first_name_furigana   {"かんなこ"}
+    last_name_furigana    {"はしのもと"}
     birthday              {"19990203"}
   end
 
