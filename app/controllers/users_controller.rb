488dd0c590cbf5635ad544b6019a287bd9f8fb52
chiwawa313 <no_pain.no_gain.0313@docomo.ne.jp>
2020-03-17 23:05:18 +0900

    update修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,12 @@ class UsersController < ApplicationController
 
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path
+    if @user = User.find(params[:id])
+       @user.update(user_params)
+       redirect_to user_path
+    else
+       render new_user_path
+    end
   end
  
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:index, :new,:show,:create,:edit,:update,:destroy]
-  resources :users, only: [:show:, :create, :update]
+  resources :users, only: [:show, :create, :update]
   resources :categorys, only: :show
   resources :identity_informations, only: [:new, :create]
   resources :shipping_addresses, only: [:new, :create]
