b51e5a77a8dc45951ad8d94680450c927c747958
yasutaka <siesta27@gmail.com>
2020-03-21 14:08:37 +0900

    商品削除完了ページの作成

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -35,3 +35,11 @@ $(function(){
     $(this).text('編集が完了いたしました').css('background-color','rgb(60,202,206)')}
 )});
 
+$(function(){
+  $('.back-to-root').hover(function(){
+    $(this).text('トップページへ戻る').css('background-color','blue')
+  },function(){
+    $(this).text('商品を削除いたしました').css('background-color','rgb(60,202,206)')}
+)});
+
+
