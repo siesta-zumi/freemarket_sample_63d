f6b6fdb462a891e366378a86de55bff696072f2c
yasutaka <siesta27@gmail.com>
2020-03-29 01:16:05 +0900

    無限増殖バグ修正

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -6,46 +6,68 @@ $(function() {
   }
   $("#item_category_id").on("change",function(){
     var int = document.getElementById("item_category_id").value;
-    $.ajax({
-      url: 'search',
-      type: 'GET',
-      dataType: 'json',
-      data: {id: int}
-    })
-    .done(function(children) {
-      var insertHTML = `<select name="child" id="child">
-                        <option value=0>選択して下さい</option>`;
-      $.each(children, function(i, child) {
-        insertHTML += buildHTML(child)
+    if(int == 0){
+      $('#child').remove();
+      $('#grand-child').remove();
+    }else{
+      $.ajax({
+        url: 'search',
+        type: 'GET',
+        dataType: 'json',
+        data: {id: int}
+      })
+      .done(function(children) {
+        var insertHTML = `<select name="child" id="child">
+                          <option value=0>---</option>`;
+        $.each(children, function(i, child) {
+          insertHTML += buildHTML(child)
+        });
+        insertHTML += `</select>`
+        if($('#child').length){
+          $('#child').replaceWith(insertHTML);
+          $('#grand-child').remove();
+        } else {
+          $('.child-category').append(insertHTML);
+  
+        };
+      })
+      .fail(function() {
       });
-      insertHTML += `</select>`
-      $('.child-category').append(insertHTML);
-    })
-    .fail(function() {
-    });
+    }
+    
   });
 
+  
   $(document).on("change","#child",function(){
-   
     var intParent = document.getElementById("item_category_id").value
     var intChild = document.getElementById("child").value
     var int = intParent + '/' + intChild
-    $.ajax({
-      url: 'search',
-      type: 'GET',
-      dataType: 'json',
-      data: {id: int}
-    })
-    .done(function(grandchild) {
-      var insertHTML = `<select name="item[category_id]" id="child_category">
-                        <option value=0>---</option>`;
-      $.each(grandchild, function(i, child) {
-        insertHTML += buildHTML(child)
+
+    if(intChild == 0){
+      $('#grand-child').remove();
+    }else{
+      $.ajax({
+        url: 'search',
+        type: 'GET',
+        dataType: 'json',
+        data: {id: int}
+      })
+      .done(function(grandchild) {
+        var insertHTML = `<select name="item[category_id]" id="grand-child">
+                          <option value=0>---</option>`;
+        $.each(grandchild, function(i, child) {
+          insertHTML += buildHTML(child)
+        });
+        insertHTML += `</select>`
+        if($('#grand-child').length){
+          $('#grand-child').replaceWith(insertHTML);
+        }else{
+          $('.grand-child-category').append(insertHTML);
+        }
+      })
+      .fail(function() {
       });
-      insertHTML += `</select>`
-      $('.grand-child-category').append(insertHTML);
-    })
-    .fail(function() {
-    });
+    }
+    
   });
 })
