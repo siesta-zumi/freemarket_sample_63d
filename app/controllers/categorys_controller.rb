680309cffcd385651f6de3be382198e0757b922c
sakozo <youlianggit@gmail.com>
2020-03-29 16:45:58 +0900

    レビュー指摘修正

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -3,8 +3,6 @@ class CategorysController < ApplicationController
   before_action :set_parents, only: [:index,:new,:show,:create, :edit]
   before_action :set_brands, only: [:index, :show]
 
-  PER = 8
-
   def index
     @parents = Category.where(ancestry: nil).limit(13)
   end
@@ -13,13 +11,11 @@ class CategorysController < ApplicationController
     @category_name = Category.find(params[:id]).name
 
     # paramsで送られてきたcategory_idと一致するかつselling_status=0(出品中)のitemを全件取得
-    @items = Item.where("category_id = :selected_category AND selling_status = 0",{selected_category: params[:id]}).page(params[:page]).per(PER).order('created_at DESC')
+    @items = Item.where("category_id = :selected_category AND selling_status = 0",{selected_category: params[:id]}).page(params[:page]).per(8).order('created_at DESC')
   end
 
   private
-
-    def set_brands
-      @brands = Brand.all
-    end
-
+  def set_brands
+    @brands = Brand.all
   end
+end
