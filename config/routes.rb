22f2b083cfb8f081662e5dbe87df225852468fa8
sakozo <youlianggit@gmail.com>
2020-03-29 18:12:36 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :users, only: [:show, :create, :update]
   resources :categorys, only: [:index, :show]
-  resources :brands, only: [:index]
+  resources :brands, only: [:index, :show]
   resources :identity_informations, only: [:new, :create]
   resources :shipping_addresses, only: [:new, :create]
   resources :orders, only: [:show, :create]
